525d0f76eb5d4e8c19792a966371a55dc469ad4f
ghost-012 <113078711+ghost-012@users.noreply.github.com>
2024-05-22 14:02:03 +0800

    Update menus.php

--- menus.php
+++ menus.php
@@ -12,9 +12,9 @@
 				<li class="nav-item"><a class="nav-link" href="brand.php" id="brand_menu">Brand</a></li>
 				<li class="nav-item"><a class="nav-link" href="supplier.php" id="supplier_menu">Supplier</a></li>
 				<li class="nav-item"><a class="nav-link" href="product.php" id="product_menu">Product</a></li>
+				<li class="nav-item"><a class="nav-link" href="purchase.php" id="purchase_menu">Purchase</a></li>
 				<li class="nav-item"><a class="nav-link" href="borrow_records.php" id="borrow_records_menu">Borrow Records</a></li>
-				<!-- <li class="nav-item"><a class="nav-link" href="purchase.php" id="purchase_menu">Purchase</a></li>
-				<li class="nav-item"><a class="nav-link" href="order.php" id="order_menu">Orders</a></li>			 -->
+				<!--<li class="nav-item"><a class="nav-link" href="order.php" id="order_menu">Orders</a></li>			-->
 			</ul>
 		</div>
 		<ul class="nav navbar-nav">
