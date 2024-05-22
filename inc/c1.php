d610e06937bc46bf6685da375643c24d4c9aec12
Ghost <113078711+ghost-012@users.noreply.github.com>
2024-05-23 07:37:21 +0800

    changes

--- inc/c1.php
+++ inc/c1.php
@@ -1,5 +1,5 @@
 </head>
-<body class="bg-gradient bg-#f5f5f5 bg-opacity-25">
+<body class="bg-gradient bg-#f5f5f5 bg-opacity-25" style="background-color: #f5f5f5;">
 <!--<nav role="navigation" class="navbar navbar-dark navbar-static-top bg-primary bg-gradient">
     <div class="container">
       <div class="navbar-header">
